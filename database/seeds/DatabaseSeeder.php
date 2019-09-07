25f85825b3ef959a53eaf6f555d7d9bd8cd511de
Bruno <brunofiliperamos@hotmail.com>
2019-09-07 03:31:37 -0300

    chore(base): repo cleared to initial laravel config

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,22 +5,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(DataRowsTableSeeder::class);
-        $this->call(DataTypesTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(MenusTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
-        $this->call(BannersTableSeeder::class);
-        $this->call(HighlightsTableSeeder::class);
-        $this->call(PositionsTableSeeder::class);
     }
 }
--- resources/views/site/partials/_footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="copyright">
-    <div class="container h-100">
-        <div class="h-100 d-flex align-items-center">
-            <span class="text-copyright">Copyright</span>
-        </div>
-    </div>
-</footer>
